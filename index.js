735b7dfbb3f8278b0c7b27265563e5295f312a95
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-13 17:16:02 +0300

    Rotated, centered, and added circle

--- index.js
+++ index.js
@@ -11,14 +11,26 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = 0;
-    const y = 0;
+    const x = width * 0.5;
+    const y = height * 0.5;
     const w = width * 0.3;
     const h = height * 0.3;
 
+    context.save();
+    context.translate(x,y);
+    context.rotate(0.3);
+
     context.beginPath();
-    context.rect(x,y,w,h);
+    context.rect(-w * 0.5, -h * 0.5, w, h);
     context.fill();
+    context.restore();
+
+    context.translate(100, 400);
+
+    context.beginPath();
+    context.arc(0, 0, 50, 0, Math.PI * 2);
+    context.fill();
+
   };
 };
 
